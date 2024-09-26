eed7f682c736349e9f0fbdc7145325dcdb728bae
B0G0N1 <michelebogoni2001@gmail.com>
2024-09-26 17:26:36 +0200

    [FEAT] Create Base Header and Main Structure

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+    <div id="jumbotron">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                SPAZIO FUMETTI
+            </div>
+        </div>
+    </div>
+@endsection
