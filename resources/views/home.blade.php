f341a1b78abe75e17d421365b5304a7dbab05766
B0G0N1 <michelebogoni2001@gmail.com>
2024-09-26 21:22:02 +0200

    [FEAT] Add Comics List

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,10 +9,25 @@
             </div>
         </div>
     </div>
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                SPAZIO FUMETTI
+
+    <div id="comics-container">
+        <div class="container">
+            <div class="row title px-4 py-2">
+                <p class="fw-bold mb-0">CURRENT SERIES</p>
+            </div>
+            <div class="row">
+                @foreach ($comics as $comic)
+                    <div class="col-12 col-md-3 col-lg-2">
+                        <div class="comic-card border-0 m-1">
+                            <div class="thumb">
+                                <img class="img-fluid" src="{{ $comic['thumb'] }}" alt="{{ $comic['series'] }}">
+                            </div>
+                            <div class="card-body">
+                                <span class="text-uppercase">{{ $comic['series'] }}</span>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
             </div>
         </div>
     </div>
