5d625be9839f60f14524f3a654ac511f2c1bbff7
B0G0N1 <michelebogoni2001@gmail.com>
2024-09-27 10:47:16 +0200

    [FEAT] Finish Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,31 +29,34 @@
                     </div>
                 @endforeach
             </div>
+            <div class="row load-more px-4 py-2">
+                <p class="fw-bold mb-0">LOAD MORE</p>
+            </div>
         </div>
     </div>
 
     <div id="adv">
-        <div class="container white">
+        <div class="container white py-5">
             <div class="row">
                 <div class="col">
                     <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="DC Comics">
                     <span class="ms-2">DIGITAL COMICS</span>
                 </div>
                 <div class="col">
-                    <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="DC Comics">
-                    <span>DIGITAL COMICS</span>
+                    <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="DC Merchandise">
+                    <span class="ms-2">DC MERCHANDISE</span>
                 </div>
                 <div class="col">
-                    <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="DC Comics">
-                    <span>DIGITAL COMICS</span>
+                    <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="DC Subscription">
+                    <span class="ms-2">SUBSCRIPTION</span>
                 </div>
                 <div class="col">
-                    <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="DC Comics">
-                    <span>DIGITAL COMICS</span>
+                    <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="DC Shop">
+                    <span class="ms-2">SHOP LOCATOR</span>
                 </div>
                 <div class="col">
-                    <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="DC Comics">
-                    <span>DIGITAL COMICS</span>
+                    <img src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="DC Visa">
+                    <span class="ms-2">DC POWER VISA</span>
                 </div>
             </div>
         </div>
